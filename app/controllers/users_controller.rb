92d359a9f42bcece2d14117446997802d79a8e42
bumpfuji10 <bumpfuji10@gmail.com>
2025-12-31 15:43:55 +0900

    [change]users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @practice_records = @user.practice_records.order(created_at: :desc)
+
+    # カレンダー用のデータ
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @start_date = @date.beginning_of_month.beginning_of_week(:sunday)
+    @end_date = @date.end_of_month.end_of_week(:sunday)
+
+    @practice_records = @user.practice_records
+      .where(created_at: @start_date.beginning_of_day..@end_date.end_of_day)
+      .group_by { |record| record.created_at.to_date }
   end
 end
